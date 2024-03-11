af365e0e6cf7bf8d9286f170edadbad74e68bcef
Samuel Silva <samuel0100wanderson@gmail.com>
2024-03-10 22:25:30 -0300

    UPDATE para vários dados

--- App.py
+++ App.py
@@ -6,7 +6,14 @@
 
 import sqlite3
 
-
+# Dicionário de Cores:
+cores = {
+    'default': '\033[m',
+    'vermelho-sub': '\033[4;31m',
+    'azul':'\033[34m',
+    'brazil':'\033[42;33m'
+}
+ 
 ### Conexão
 caminho = ".\\db_Autopecas.db"
 
@@ -27,20 +34,11 @@ def criarTabela(conexao, comando):
         cursor = conexao.cursor()
         cursor.execute(comando)
         
-        print('Tabela criada com sucesso')
+        print('{}Tabela criada com sucesso{}'.format(cores['azul'], cores['default']))
     except sqlite3.Error as er:
         print(er)
 
 # INSERÇÃO DE DADOS
-def inserirDados(conexao, comando):
-    try:
-        cursor = conexao.cursor()
-        cursor.execute(comando)
-        conexao.commit()
-
-        print("Dados inseridos com sucesso!")
-    except sqlite3.Error as er:
-        print(er)
 
 def preencher():
     print(f'\n{"PREENCHENDO":=^20}')
@@ -51,6 +49,25 @@ def preencher():
         float(input('VALOR -> '))
     ]
     return campo
+
+def inserirDados(conexao):
+    campos = preencher()
+
+    comando = f"""
+    INSERT INTO TB_PECAS
+    (NOME, DESCRICAO, FAMILIA, VALOR) VALUES
+    ('{campos[0]}', '{campos[1]}', '{campos[2]}', {campos[3]});
+    """
+    
+    try:
+        cursor = conexao.cursor()
+        cursor.execute(comando)
+        conexao.commit()
+
+        print("{}Dados inseridos com sucesso!{}".format(cores['azul'], cores['default']))
+
+    except sqlite3.Error as er:
+        print(er)
 
 # "TRUNCATE"
 comandoTruncar = """
@@ -67,7 +84,7 @@ def truncar(conexao):
 
 # DELETAR LINHA ESPECIFICA
 def deletarlinha(conexao):
-    linha = int(input('Qual o ID do produto deseja excluir? '))
+    linha = int(input('Qual o {}ID{} do produto deseja excluir? '.format(cores['vermelho-sub'], cores['default'])))
     cursor = conexao.cursor()
     comando = f"""
 DELETE FROM TB_PECAS
@@ -91,86 +108,124 @@ def dropTable(conexao):
         print(er)
 
 # Atualizar uma célula
-def update(conexao):
-    print(f"\n{'UPDATE':=^25}")
+def update(conexao, qnt):
+    print("\n{}{:=^25}{}".format(cores['brazil'], 'UPDATE', cores['default']))
     
-    id = int(input('Qual o ID da linha que deseja alterar o valor -> '))
+    ordem = 1
 
-    while True:
-        print(f'{"Qual a coluna":=^25}')
-        print("""
-        [1] NOME
-        [2] DESCRICAO
-        [3] FAMÍLIA
-        [4] VALOR""")
+    while ordem <= qnt:
+        if qnt > 1:
+            print(f'==={ordem}º Update===')
+        
+        ordem += 1
 
-        numColuna = int(input('\nSUA RESPOSTA -> '))
+        id = int(input('Qual o {}ID{} da linha que deseja alterar o valor -> '.format(cores['vermelho-sub'], cores['default'])))
 
-        if numColuna == 1:
-            nomeColuna = 'nome'
-            break
-        elif numColuna == 2:
-            nomeColuna = 'descricao'
-            break
-        elif numColuna == 3:
-            nomeColuna = 'familia'
-            break
-        elif numColuna == 4:
-            nomeColuna = 'valor'
-            break
-        else:
-            print(f'{"Resposta Inválida":=^25}')
-    
-    if nomeColuna != 'valor':
-        dado = str(input('VALOR NOVO -> '))
-    else:
         while True:
-            try:
-                dado = float(input('VALOR NOVO -> '))
+            print(f'{"Qual a coluna":=^25}')
+            print("""
+            [1] NOME
+            [2] DESCRICAO
+            [3] FAMÍLIA
+            [4] VALOR""")
+
+            numColuna = int(input('\nSUA RESPOSTA -> '))
+
+            if numColuna == 1:
+                nomeColuna = 'nome'
                 break
+            elif numColuna == 2:
+                nomeColuna = 'descricao'
+                break
+            elif numColuna == 3:
+                nomeColuna = 'familia'
+                break
+            elif numColuna == 4:
+                nomeColuna = 'valor'
+                break
+            else:
+                print(f'{"Resposta Inválida":=^25}')
+        
+        if nomeColuna != 'valor':
+            dado = str(input('VALOR NOVO -> '))
+        else:
+            while True:
+                try:
+                    dado = float(input('VALOR NOVO -> '))
+                    break
 
-            except ValueError:
-                print('Digite apenas valores numéricos!')
-                print(f'{"TENTE NOVAMENTE":-^25}') 
+                except ValueError:
+                    print('Digite apenas valores numéricos!')
+                    print(f'{"TENTE NOVAMENTE":-^25}') 
 
-    comandoUpdate = f"""
-    UPDATE TB_PECAS
-    SET {nomeColuna} = '{dado}'
-    WHERE rowid = {id};"""
+        comandoUpdate = f"""
+        UPDATE TB_PECAS
+        SET {nomeColuna} = '{dado}'
+        WHERE rowid = {id};"""
 
+        try:
+            cursor = conexao.cursor()
+            cursor.execute(comandoUpdate)
+            conexao.commit()
+        except sqlite3.Error as er:
+            print(er)
+
+### Menu
+def menu(conexao):
+    print(f"""
+    {'MENU':=^20}
+    [1] INSERIR DADOS
+    [2] ATUALIZAR DADO
+    [3] DELETAR LINHA
+    
+    [0] Sair""")
+    
     try:
-        cursor = conexao.cursor()
-        cursor.execute(comandoUpdate)
-        conexao.commit()
-    except sqlite3.Error as er:
-        print(er)
+        resp = int(input('SUA RESPOSTA -> '))
+        if resp == 1:
+            inserirDados(conexao)
+        elif resp == 2:
+            print('\nESCOLHA UMA OPÇÃO:')
+            print('[1] Atualizar apenas um dado')
+            print('[2] Atualizar vários dados')
+            opcao = int(input('\nResposta -> '))
+
+            if opcao == 1:
+                update(conexao, 1)
+            else:
+                qnt = int(input('Quantos dados deseja atualizar? '))
+                update(conexao, qnt)
+
+        elif resp == 3:
+            deletarlinha(conexao)
+        elif resp == 0:
+            exit()
+        elif resp == 16082003:
+            truncar(conexao)           #Comando oculto
+        else:
+            print('\n=== Valor inválido. Tente novamente!===\n')
+            menu(conexao)
+
+    except ValueError as ve:
+        print('\n=== Valor inválido. Tente novamente!===\n')
+        menu(conexao)
 
 ### Iniciando procedimentos
-comandoCriarTabela = """
-CREATE TABLE TB_PECAS(
-	CODIGO INTEGER PRIMARY KEY AUTOINCREMENT,
-	NOME varchar(50),
-	DESCRICAO TEXT,
-	FAMILIA TEXT,
-	VALOR DECIMAL(12,2)
-    )"""
 
 # Estabelecer conexão
 vcon = conectar(caminho) 
 
-# criarTabela(vcon, comandoCriarTabela) # Se já existe será printado "Already Exists"
+# comandoCriarTabela = """
+# CREATE TABLE TB_PECAS(
+# 	CODIGO INTEGER PRIMARY KEY AUTOINCREMENT,
+# 	NOME varchar(50),
+# 	DESCRICAO TEXT,
+# 	FAMILIA TEXT,
+# 	VALOR DECIMAL(12,2)
+#     )"""
 
-### INSERINDO DADOS
-# campos = preencher()
+# criarTabela(vcon, comandoCriarTabela)
+# Se já existe será printado "Already Exists"
 
-# comandoInserir = f"""
-# INSERT INTO TB_PECAS
-# (NOME, DESCRICAO, FAMILIA, VALOR) VALUES
-# ('{campos[0]}', '{campos[1]}', '{campos[2]}', {campos[3]});
-# """
-
-# inserirDados(vcon, comandoInserir)
-
-update(vcon)
-
+# menu(vcon)
 vcon.close()
